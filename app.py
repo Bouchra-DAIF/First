bc0f7e227655d2eeeeffa6fae1d8059b3d2aa47d
Bouchra DAIF <114958090+Bouchra-DAIF@users.noreply.github.com>
2022-10-05 00:44:36 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+def my_function () :
+ d = input("Entrez la densité : ")
+ VC = input("Entrez la valeur de la viscosité Cénimatique en St :")
+ try:
+    d = float(d)
+    VC = float(VC)
+    u = VC*1000*d*10**-4
+    print("La viscosité dynamique est : ",u, "PI")
+ except:
+    print("la valeur de la densité et/ou la viscosité cénimatique")
+my_function ()
